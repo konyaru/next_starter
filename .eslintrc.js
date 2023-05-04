4202752731b52323ce0161b31bc2a082193704ad
konyaru <orira4666@gmail.com>
2023-05-04 09:43:39 +0900

    add: .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: { browser: true, node: true, es2021: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-unused-vars': ['off'],
+    '@typescript-eslint/ban-ts-comment': ['off'],
+    '@typescript-eslint/no-non-null-assertion': ['off'],
+    'prefer-const': ['off'],
+    'no-irregular-whitespace': ['off'],
+  },
+}
