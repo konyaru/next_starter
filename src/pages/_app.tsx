56b61cb85ec672c2c39a80449077d3dfd61415f1
konyaru <orira4666@gmail.com>
2023-05-06 17:29:43 +0900

    refactor: ライブラリのimport文を並び替え

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import Layout from '@/components/common/Layout'
 import AppProvider from '@/provider/app'
 import '@/styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
